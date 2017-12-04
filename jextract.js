f3d369da6325a2f1dceb1b61e700c571ea994713
Mikhail Kormanowsky <mkmkor-kr@yandex.ru>
2017-12-05 00:35:07 +0300

    Update jextract.js

--- jextract.js
+++ jextract.js
@@ -1,47 +1,44 @@
-/** jExtraxct extracts information from DOM using jQuery */
-/** Example:
-  DOM:
-    <span id="user-name">John</span>
-    <span id="user-age">27</span>
-  JS:
-    var data = jExtract({
-    name: "#user-name",
-    age: "#user-age"
-  }); // data = {name: "John", age: "27"}
-*/
-function jExtract(struct){
-  struct = struct || {};
-  $.each(struct, function(i, e){
-    var r; switch(typeof e){
-      case "string":
-      r = $(e).text().trim();
-      break;
-      case "object":
-      if(e instanceof Array){
-        var a = [];
-        if(e.length == 1 && typeof e[0] === "string"){
-          $(e[0]).each(function(x, y){
-            a.push($(y).text().trim()); });
-          }else if(e.length == 2 && typeof e[0] === "string" && typeof e[1] === "object"){
-            $(e[0]).each(function(x, y){
-              var b = {};
-              $.each(e[1], function(i, j){
-                b[i] = $(y).find(j).text().trim();
-              });
-              a.push(b);
-            });
-          }else{
-            e.forEach(function(b, c, d){
-              a.push($(b).text().trim());
-            });
-          }
-          r = a;
-        }else{
-          r = jExtract(e);
-        }
-        break;
-      } struct[i] = r;
-    });
-    return struct;
+function jExtract(struct, parent){
+  //Throw an error about jQuery
+  if(typeof $ !== "function"){
+    throw 'jExtract error: jExtract depends on jQuery, but it wasn`t loaded';
+    return false;
   }
-
+  //Some default values...
+  struct = struct || {};
+  parent = parent || $("html");
+  //...and useful functions
+  var $g = function(selector){
+    return parent.find(selector);
+  }, $e = function(key){
+    throw 'jExtract error: key "' + key + '" in your struct is of type "' + typeof struct[key] + '" but only strings and objects are allowed.';
+  }, data = {};
+  //Start our loop
+  $.each(struct, function(i, e){
+    //Very simple case: just replace selector with text
+    if(typeof e == "string"){
+      data[i] = $g(e).text().trim();
+    }else if(typeof e == "object"){
+      if(e instanceof Array){
+        //if it's array, check how much elements it has.
+        //We support only arrays like [string] to get texts of all elements with selector
+        //and [string, object] to extract children structure inside an each element with selector
+        data[i] = [];
+        $g(e[0]).each(function(j, f){
+          if(e.length == 1){
+            data[i].push($(f).text().trim());
+          }else if(e.length == 2){
+            data[i].push(jExtract(e[1], $(f)));
+          }
+        });
+      }else if(e !== null){
+        //if it's object, just call jExtract for this object
+        data[i] = jExtract(e, parent);
+      }
+    }else{
+      $e(i);
+    }
+  });
+  //Return structure filled in with data
+  return data;
+}
